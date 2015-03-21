bcb4ac76f1dc6d7e30adb819f750f436eb6dc5f2
Max Mizikar <maxmzkr@gmail.com>
2015-03-20 23:16:16 -0400

    actually sourcing the git stuff

--- walk_this_folder.sh
+++ walk_this_folder.sh
@@ -3,3 +3,4 @@ source /home/max/.bash/betty.sh
 source /home/max/.bash/user-bin.sh
 source /home/max/.bash/go/go.sh
 source /home/max/.bash/deepfield/virtual_evn.sh
+source /home/max/.bash/git/git_branch.sh
