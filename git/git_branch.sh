1e51b00f6ee28955c06c2c7c8aee1ea9bc709378
Max Mizikar <maxmzkr@gmail.com>
2015-03-23 12:57:37 -0400

    Remove a space when there is no branch, add it back when there is

--- git/git_branch.sh
+++ git/git_branch.sh
@@ -3,6 +3,9 @@
 # Adds the git branch to the terminal's prompt
 
 parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+  branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')
+  if [[ -n "${branch}" ]]; then
+    echo " $branch"
+  fi
 }
-PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch) $ "
+PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch)$ "
