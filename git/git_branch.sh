bc99d9765d470bfa8cd25349bef5c1d86fe8f1af
Max Mizikar <maxmzkr@gmail.com>
2015-04-04 15:01:34 -0400

    Added the things

--- git/git_branch.sh
+++ git/git_branch.sh
@@ -5,4 +5,4 @@
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch) $ "
+PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch)$ "
